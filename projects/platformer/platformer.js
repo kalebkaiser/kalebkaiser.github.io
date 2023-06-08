6984fb428bed329318a68af8f7b30cb58d5421ee
kalebkaiser kaiser <kalebakaiser@gmail.com>
2023-06-08 16:53:36 +0000

    <message about commit>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -47,9 +47,9 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
-
-
+    createPlatform(500, 300, 200, 20); 
+    createPlatform(300,610, 200, 20)
+     createPlatform( 90,700,200,20)
     
     // TODO 2
     // Create collectables
